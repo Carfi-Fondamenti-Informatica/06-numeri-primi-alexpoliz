5f1f6f270a232d2472c748e94542a4e9d6ddb244
alexpoliz <117836399+alexpoliz@users.noreply.github.com>
2022-12-03 10:57:00 +0100

    Update lib.cpp

--- lib.cpp
+++ lib.cpp
@@ -1,1 +1,17 @@
-
+bool scm(int a, int b) {
+  a=abs(aa);
+  b=abs(b);
+  if (a==0 || a==1)
+    return false;
+  int rest = a % --b;
+  if rest==0 && b>1){
+    return false;
+  }else if(b!=1)
+    return scm(a,b);
+  else 
+    return true;
+}
+      
+      
+      
+  
