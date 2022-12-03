f97474ce270c03a254174cafcc6afa7f83df564d
alexpoliz <117836399+alexpoliz@users.noreply.github.com>
2022-12-03 10:32:36 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,5 +1,13 @@
 #include <iostream>
-
+include "dec.h"
+  using namespace std;
 int main(){
+  int a;
+  cin >> a;
+  if (scm(a,a)){
+    cout << "numero primo";
+  }else { 
+    cout << "numero non primo";
+  }
   return 0;
 }
